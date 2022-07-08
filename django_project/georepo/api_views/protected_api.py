5a0804ba8b44c431d1a5bcf6909630b815d4700d
dimasciput <dimas.ciputra@gmail.com>
2022-07-08 09:21:16 +0700

    Check dataset

--- django_project/georepo/api_views/protected_api.py
+++ django_project/georepo/api_views/protected_api.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from rest_framework.views import APIView
+
+from georepo.models import Dataset
 
 
 class ProtectedApi(APIView):
@@ -8,4 +10,15 @@ class ProtectedApi(APIView):
         return HttpResponse('Ok')
 
     def post(self, request, *args, **kwargs):
+        request_url = request.query_params.get('request_url', '')
+        try:
+            params = list(filter(None, request_url.split('/')))
+            dataset_label = params[1]
+            entity_type_label = params[2]
+
+            dataset = Dataset.objects.filter(
+                label=dataset_label
+            )
+        except IndexError:
+            return HttpResponseForbidden()
         return HttpResponse('Ok')
